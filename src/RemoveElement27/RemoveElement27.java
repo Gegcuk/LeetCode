e9fad17606ce6b1aaa976da0911f997042a228fa
Aleksei Lazunin <gegcuk@gmail.com>
2024-06-24 02:06:15 +0100

    27. Remove Element is Done

---
+++ src/RemoveElement27/RemoveElement27.java
@@ -0,0 +1,36 @@
+package RemoveElement27;
+
+import java.util.Arrays;
+
+public class RemoveElement27 {
+
+
+    public int removeElement(int[] nums, int val) {
+        int index = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement27 solution = new RemoveElement27();
+
+        // Test case 1
+        int[] nums1 = {0,1,2,2,3,0,4,2};
+        int val1 = 2;
+        int k1 = solution.removeElement(nums1, val1);
+        System.out.println("Output: " + k1 + ", nums = " + Arrays.toString(Arrays.copyOf(nums1, k1)));
+
+        // Test case 2
+        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val2 = 2;
+        int k2 = solution.removeElement(nums2, val2);
+        System.out.println("Output: " + k2 + ", nums = " + Arrays.toString(Arrays.copyOf(nums2, k2)));
+    }
+
+}
+
