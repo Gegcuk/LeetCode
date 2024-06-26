37dd7bafd182e72aa3a015e00473028d99f1c005
Aleksei Lazunin <gegcuk@gmail.com>
2024-06-27 00:01:17 +0100

    118. Pascal's Triangle II is done

---
+++ src/Easy/P119PascalTriangleII/Solution.java
@@ -0,0 +1,29 @@
+package Easy.P119PascalTriangleII;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> currentRow = new ArrayList<>();
+        currentRow.add(1);
+        if(rowIndex == 0) return currentRow;
+        currentRow.add(1);
+        if(rowIndex == 1) return currentRow;
+        for(int i = 0; i < rowIndex-1; i++){
+            currentRow = nextRow(currentRow);
+        }
+        return currentRow;
+    }
+
+    public static List<Integer> nextRow(List<Integer> currentRow){
+        List<Integer> nextRow = new ArrayList<>();
+        nextRow.add(1);
+        for(int i = 1; i < currentRow.size(); i++){
+            nextRow.add(currentRow.get(i) + currentRow.get(i-1));
+        }
+        nextRow.add(1);
+        return nextRow;
+    }
+}
