1c2c86937bc7f964adadaefa02a4315cc997bf71
Aleksei Lazunin <gegcuk@gmail.com>
2024-07-01 13:38:20 +0100

    2. Add two numbers is done

---
+++ src/Easy/P28FindtheIndexoftheFirstOccurrenceinaString/Solution.java
@@ -0,0 +1,7 @@
+package Easy.P28FindtheIndexoftheFirstOccurrenceinaString;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+       return haystack.indexOf(needle);
+    }
+}
---
+++ src/Medium/P2AddTwoNumbers/Solution.java
@@ -0,0 +1,58 @@
+package Medium.P2AddTwoNumbers;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0){
+            int digit1 = (l1 != null) ? l1.val : 0;
+            int digit2 = (l2 != null) ? l2.val : 0;
+
+            int sum = digit1 + digit2 + carry;
+            int digit = sum % 10;
+            carry = sum/10;
+
+            tail.next = new ListNode(digit);
+            tail = tail.next;
+
+            l1 = (l1 != null) ? l1.next : null;
+            l2 = (l2 != null) ? l2.next : null;
+        }
+        return head.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Create test list1: 2 -> 4 -> 3 (represents 342)
+        ListNode list1 = new ListNode(2);
+        list1.next = new ListNode(4);
+        list1.next.next = new ListNode(3);
+
+        // Create test list2: 5 -> 6 -> 4 (represents 465)
+        ListNode list2 = new ListNode(5);
+        list2.next = new ListNode(6);
+        list2.next.next = new ListNode(4);
+
+        // Add the two lists
+        ListNode resultList = solution.addTwoNumbers(list1, list2);
+
+        // Print the result list
+        System.out.print("Result List: ");
+        while (resultList != null) {
+            System.out.print(resultList.val + " ");
+            resultList = resultList.next;
+        }
+    }
+}
