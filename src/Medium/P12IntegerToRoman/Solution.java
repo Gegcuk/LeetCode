0650b9de3d08d57806b957c346c4b4c032a356f1
Aleksei Lazunin <gegcuk@gmail.com>
2025-02-24 22:25:51 +0000

    12. Integer to Roman is solved

--- src/Medium/P12IntegerToRoman/Solution.java
+++ src/Medium/P12IntegerToRoman/Solution.java
@@ -5,13 +5,28 @@ import java.util.Map;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
+        StringBuilder resultStringBuilder = new StringBuilder();
+
+        for(int i = 0; i < values.length; i++){
+            if(num == 0) break;
+            while(num >= values[i]){
+                resultStringBuilder.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+        return resultStringBuilder.toString();
     }
 
-        public String getWrongAnswers(int N, String C) {
-            // Write your code here
-            return "";
-        }
 
+    public static void main(String[] args) {
+        System.out.println(intToRoman(5));
+        System.out.println(intToRoman(10));
+        System.out.println(intToRoman(34));
+        System.out.println(intToRoman(36));
+        System.out.println(intToRoman(149));
+    }
 }
