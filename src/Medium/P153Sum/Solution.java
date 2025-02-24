bbaf61e808e18fe8610b7b1b0ae5f5df36de5f0e
Aleksei Lazunin <gegcuk@gmail.com>
2025-02-24 23:58:54 +0000

    15. 3Sum is solved

---
+++ src/Medium/P153Sum/Solution.java
@@ -0,0 +1,44 @@
+package Medium.P153Sum;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        List<List<Integer>> allRes = threeSum(new int[]{-1,0,1,2,-1,-4});
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) return new ArrayList<>();
+        Arrays.sort(nums);
+
+        return IntStream.range(0, nums.length - 2)
+                // Skip duplicates for the first element.
+                .filter(i -> i == 0 || nums[i] != nums[i - 1])
+                .boxed()
+                .flatMap(i -> {
+                    List<List<Integer>> localResult = new ArrayList<>();
+                    for (int lo = i + 1, hi = nums.length - 1; lo < hi;) {
+                        int sum = nums[i] + nums[lo] + nums[hi];
+                        if (sum == 0) {
+                            localResult.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                            // Skip duplicate values for lo and hi.
+                            while (lo < hi && nums[lo] == nums[lo + 1]) lo++;
+                            while (lo < hi && nums[hi] == nums[hi - 1]) hi--;
+                            lo++;
+                            hi--;
+                        } else if (sum < 0) {
+                            lo++;
+                        } else {
+                            hi--;
+                        }
+                    }
+                    return localResult.stream();
+                })
+                .collect(Collectors.toList());
+    }
+}
+
