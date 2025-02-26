9a453414e48924c2f80eea8f453509795bc48762
Aleksei Lazunin <gegcuk@gmail.com>
2025-02-26 00:25:00 +0000

    29. Divide Two Integers is solved.

---
+++ src/Medium/P29DivideTwoIntegers/Solution.java
@@ -0,0 +1,24 @@
+package Medium.P29DivideTwoIntegers;
+
+import java.util.stream.IntStream;
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        long quotientAccumulator = 0;
+
+        if (divisor == 1) return dividend;
+        long resultSign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
+
+        long absoluteDividend = Math.abs((long) dividend);
+        long absoluteDivisor = Math.abs((long) divisor);
+
+        for (int shiftIndex = 30; shiftIndex >= 0; shiftIndex--) {
+            if (absoluteDividend >= (absoluteDivisor << shiftIndex)) {
+                quotientAccumulator += (1L << shiftIndex);
+                absoluteDividend -= (absoluteDivisor << shiftIndex);
+            }
+        }
+
+        return (int) (quotientAccumulator * resultSign);
+    }
+}
