59ab4a52618fa9bad67467d1678dff1f37bd63a4
Aleksei Lazunin <gegcuk@gmail.com>
2025-03-09 18:59:08 +0000

    2269 Find The K Beauty Of A Number

---
+++ src/Easy/P2269FindTheKBeautyOfANumber/Solution.java
@@ -0,0 +1,30 @@
+package Easy.P2269FindTheKBeautyOfANumber;
+
+public class Solution {
+
+    public int divisorSubstrings(int num, int k) {
+
+        String numbetString = String.valueOf(num);
+        int start = 0, end = start + k;
+        int counter = 0;
+        while (end <= numbetString.length()){
+            int curSubInt = Integer.parseInt(numbetString.substring(start, end));
+            if(curSubInt == 0) {
+                start++;
+                end++;
+                continue;
+            }
+            if(num % curSubInt == 0) counter++;
+            start++;
+            end++;
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.divisorSubstrings(430043, 2);
+    }
+}
