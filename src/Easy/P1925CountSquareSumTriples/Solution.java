1ab1203f25cd0273355121629db3fd269fea025e
Aleksei Lazunin <gegcuk@gmail.com>
2025-03-10 11:17:56 +0000

    1925 Count Square Sum Triples

---
+++ src/Easy/P1925CountSquareSumTriples/Solution.java
@@ -0,0 +1,37 @@
+package Easy.P1925CountSquareSumTriples;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int countTriples(int n) {
+
+        int[] squares = new int[n];
+        for(int i = 1; i <= squares.length; i++){
+            squares[i-1] = i*i;
+        }
+        System.out.println(Arrays.toString(squares));
+
+        int counter = 0;
+        int currentNumber;
+        for(int i = 0; i < squares.length; i++){
+            currentNumber = squares[i];
+            for(int j = 0; j < i; j++){
+                for(int k = 0; k < j; k++){
+                    if(squares[k] + squares[j] == currentNumber) {
+                        if(k != j) counter += 2;
+                        else counter++;
+                    }
+                }
+            }
+        }
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countTriples(10));
+    }
+
+}
