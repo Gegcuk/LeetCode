e640703a2f5cf4dd962d80bc3637ee24593603dc
Aleksei Lazunin <gegcuk@gmail.com>
2025-03-11 10:13:06 +0000

    1323 Maximum 69 number

---
+++ src/Easy/P1323Maximum69Number/Solution.java
@@ -0,0 +1,25 @@
+package Easy.P1323Maximum69Number;
+
+public class Solution {
+    public int maximum69Number (int num) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(num);
+
+        for(int i = 0; i < stringBuilder.length(); i++){
+            if(stringBuilder.charAt(i) == '6'){
+                stringBuilder.setCharAt(i, '9');
+                break;
+            }
+        }
+
+        return Integer.parseInt(stringBuilder.toString());
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.maximum69Number(9669));
+    }
+}
