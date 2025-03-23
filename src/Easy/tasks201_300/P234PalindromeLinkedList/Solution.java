4461973bd306093e1e7c4e695d9af3a19c7a76b9
alekseiLazunin <gegcuk@gmail.com>
2025-03-23 18:30:22 +0000

    234 Palindrome linked list

--- src/Easy/tasks201_300/P234PalindromeLinkedList/Solution.java
+++ src/Easy/tasks201_300/P234PalindromeLinkedList/Solution.java
@@ -7,7 +7,8 @@ public class Solution {
 
     public boolean isPalindrome(ListNode head) {
         if (head == null) return false;
-        if  (head.val != head.next.val) return false;
+        if (head.next == null) return true;
+        if  ((head.next.next == null) && (head.val != head.next.val)) return false;
         int nodesLength = 0;
         ListNode tempHead = head;
         while(tempHead != null) {
@@ -20,15 +21,22 @@ public class Solution {
             nodeValsArray[i] = tempHead.val;
             tempHead = tempHead.next;
         }
-        int right, left = 0;
+        int right, left;
         int mid = nodesLength/2;
-        right = mid % 2 == 0 ? mid + 1 : mid + 2;
+        right = nodesLength % 2 == 0 ? mid: mid + 1;
+        left = mid-1;
         while(left >= 0 && right < nodesLength) {
             if(nodeValsArray[left] != nodeValsArray[right]) return false;
             left--;
             right++;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode listNode = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(2, new ListNode(1)))));
+        System.out.println(solution.isPalindrome(listNode));
     }
 
 }
